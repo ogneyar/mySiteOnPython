228fd2b22794c6dd21ae8bcb0a9dc684f62074d3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-08-24 14:11:51 +0300

    Update my.py

--- cgi-bin/my.py
+++ cgi-bin/my.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-print("Content-type: text/html")
-print()
-print("<br><br><br><center><h1>Hell Work!</h1></center>")
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [b'<br><br><br><center><h1>HellyWrd</h1></center>']
