9f622dc6fdccb1db48fca27784c18afaa6179b71
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-08-24 14:13:39 +0300

    Update my.py

--- cgi-bin/my.py
+++ cgi-bin/my.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     return [b'<br><br><br><center><h1>HellyWrd</h1></center>']
