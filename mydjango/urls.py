0f3241c7679ea3b4521f27618cec2bd36ee57322
ogneyar <ogneyar-ne@ya.ru>
2020-11-18 22:16:45 +0300

    www

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path, include
-
-from django.contrib import admin
-
-admin.autodiscover()
-
 import source.views
 
 
 urlpatterns = [
-    path("", source.views.index, name="index"),
-    path("admin/", admin.site.urls),
+    path("", source.views.index, name="index")
 ]
